1374d2f162fe634a5c41721e0afb6af97998ccdb
Quanechhh <quan16143@gmail.com>
2025-06-17 22:56:19 +0700

    BTL_Admin_6

---
+++ app/src/admin/java/com/example/tlulostandclaim/ui/CategoryManagementFragment.java
@@ -0,0 +1,81 @@
+package com.example.tlulostandclaim.ui;
+
+import android.annotation.SuppressLint;
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.lifecycle.ViewModelProvider;
+import androidx.navigation.NavController;
+import androidx.navigation.fragment.NavHostFragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+
+import android.os.Handler;
+import android.os.Looper;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.tlulostandclaim.R;
+import com.example.tlulostandclaim.databinding.FragmentAdminUserManagementBinding;
+import com.example.tlulostandclaim.databinding.FragmentCategoryManagementBinding;
+import com.example.tlulostandclaim.utils.GlobalData;
+import com.example.tlulostandclaim.utils.GlobalFunction;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class CategoryManagementFragment extends Fragment {
+
+    private NavController navController;
+    private FragmentCategoryManagementBinding binding;
+    private final Handler handler = new Handler(Looper.getMainLooper());
+    private Runnable workRunnable;
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        navController = NavHostFragment.findNavController(CategoryManagementFragment.this);
+        return binding.getRoot();
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        binding.toolbarSearch.imageBack.setOnClickListener(v -> navController.navigateUp());
+        binding.toolbarSearch.textToolbarTitle.setText("Quản lý danh mục");
+        binding.listOfCategory.setLayoutManager(new LinearLayoutManager(requireContext()));
+        binding.inputSearch.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                if (workRunnable != null) handler.removeCallbacks(workRunnable);
+                extracted(s);
+                handler.postDelayed(workRunnable, 300);
+            }
+
+            private void extracted(CharSequence s) {
+                workRunnable = () -> performSearch(s.toString());
+            }
+
+            private void performSearch(String string) {
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
+            }
+        });
+
+        
+    }
+
+    
+}
