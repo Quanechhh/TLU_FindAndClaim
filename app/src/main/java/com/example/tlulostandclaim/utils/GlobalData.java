df70fd5dc1362000e0e079585243cdf172c664a5
vanrap136 <rap13062003@gmail.com>
2025-06-17 21:18:07 +0700

    cap nhat GlobalData

--- app/src/main/java/com/example/tlulostandclaim/utils/GlobalData.java
+++ app/src/main/java/com/example/tlulostandclaim/utils/GlobalData.java
@@ -1,9 +1,15 @@
 package com.example.tlulostandclaim.utils;
 
+import com.example.tlulostandclaim.data.model.User;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.firestore.FirebaseFirestore;
 
 public class GlobalData {
-    public static FirebaseAuth firebaseAuth = FirebaseAuth.getInstance();
-    public static FirebaseFirestore firebaseDB = FirebaseFirestore.getInstance();
+    public static final FirebaseAuth firebaseAuth = FirebaseAuth.getInstance();
+    public static final FirebaseFirestore firebaseDB = FirebaseFirestore.getInstance();
+    public static final String needToFillAllFields = "Bạn cần nhập đủ các trường";
+    public static final String commonError = "Đã có lỗi xảy ra, vui lòng thử lại!";
+    public static final String commonSuccess = "Thành công!";
+
+    public static User user;
 }
