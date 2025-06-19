bddc2ee4ea747bf57bd80647774a849f39fa56b7
dungtienvu <vutiendung2866@gmail.com>
2025-06-19 21:05:05 +0700

    thêm chức năng tìm kiếm và cập nhật trạng thái yêu cầu nhận đồ

--- app/src/employee/java/com/example/tlulostandclaim/student_request/StudentRequestManagementViewModel.java
+++ app/src/employee/java/com/example/tlulostandclaim/student_request/StudentRequestManagementViewModel.java
@@ -6,15 +6,25 @@ import androidx.lifecycle.ViewModel;
 
 import com.example.tlulostandclaim.data.model.RequestLostItemModel;
 import com.example.tlulostandclaim.utils.GlobalData;
+import com.example.tlulostandclaim.utils.GlobalFunction;
 import com.google.firebase.firestore.QueryDocumentSnapshot;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+
 public class StudentRequestManagementViewModel extends ViewModel {
     private MutableLiveData<List<RequestLostItemModel>> _itemsResponse = new MutableLiveData<>();
 
     public LiveData<List<RequestLostItemModel>> getItemsResponse() {
         return _itemsResponse;
+    }
+
+    private MutableLiveData<Boolean> _updateRequestResponse = new MutableLiveData<>();
+
+    public LiveData<Boolean> updateRequestResponse() {
+        return _updateRequestResponse;
     }
 
     private MutableLiveData<RequestLostItemModel> _itemResponse = new MutableLiveData<>();
@@ -49,6 +59,31 @@ public class StudentRequestManagementViewModel extends ViewModel {
                     if (task.isSuccessful()) {
                         RequestLostItemModel model = task.getResult().toObject(RequestLostItemModel.class);
                         if (model != null) _itemResponse.setValue(model);
+                    }
+                });
+    }
+
+    void searchItems(String text) {
+        List<RequestLostItemModel> list = new ArrayList<>();
+        for (RequestLostItemModel i : originalLostItemResponse) {
+            if (i.getItemName().contains(text) || i.getUserPhone().contains(text) || i.getUserName().contains(text)) {
+                list.add(i);
+            }
+        }
+        _itemsResponse.setValue(list);
+    }
+
+    public void updateRequestStatus(int status, String id) {
+        // 1 - agree, 2 - decline
+        Map<String, Object> hm = new HashMap<>();
+        hm.put("status", status);
+        GlobalData.firebaseDB.
+                collection("request_lost_items").document(id).update(hm)
+                .addOnCompleteListener(task -> {
+                    if (task.isSuccessful()) {
+                        _updateRequestResponse.setValue(true);
+                    } else {
+                        _updateRequestResponse.setValue(false);
                     }
                 });
     }
